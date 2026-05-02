2f84dbe1dffd36a97026c7c6a88d4bf97e79727d
sanjidtk <sanjidmon@gmail.com>
2026-05-02 10:34:36 +0400

    Fix: Hardcoded Client ID fallback for CMS auth

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
   const host = req.headers.host;
   const protocol = req.headers['x-forwarded-proto'] || 'http';
-  const url = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo,user&redirect_uri=${protocol}://${host}/api/callback`;
+  const clientId = process.env.GITHUB_CLIENT_ID || 'Ov23lip6m5bsVmLQCGVL';
+  const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo,user&redirect_uri=${protocol}://${host}/api/callback`;
   res.redirect(url);
 }
